c7a2793c893f521ec69301cbdd00e1f48739b175
François Beausoleil <francois@teksol.info>
2025-03-29 09:42:53 -0400

    Flush db before seeding in development
    This makes it easier to work!

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,51 +33,53 @@ def next_sunday_on_or_after(datetime)
   datetime
 end
 
-Attendance.delete_all
-Registration.delete_all
-Event.delete_all
-Season.delete_all
+ApplicationRecord.transaction do
+  Attendance.delete_all
+  Registration.delete_all
+  Event.delete_all
+  Season.delete_all
 
-tz = ActiveSupport::TimeZone["America/Montreal"]
+  tz = ActiveSupport::TimeZone["America/Montreal"]
 
-seasons = 2.times.map { Date.today.year + _1 }.map do |start_year|
-  Season.create!(name: "#{start_year}-#{start_year.succ}", slug: SLUGS.shift)
-end
+  seasons = 2.times.map { Date.today.year + _1 }.map do |start_year|
+    Season.create!(name: "#{start_year}-#{start_year.succ}", slug: SLUGS.shift)
+  end
 
-seasons.each do |season|
-  season.events.create!(title: "Vert & Or", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 8, 29, 10)), slug: SLUGS.shift, max_registrations: 12).tap { seed_registrations(4, _1) }
-  season.events.create!(title: "Vert & Or", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 9, 10, 10)), slug: SLUGS.shift, max_registrations: 12).tap { seed_registrations(8, _1) }
-  season.events.create!(title: "Vert & Or", start_at: next_saturday_on_or_after(tz.local(season.name.to_i, 10, 1, 10)), slug: SLUGS.shift, max_registrations: 12)
-  season.events.create!(title: "Vert & Or", start_at: next_saturday_on_or_after(tz.local(season.name.to_i, 10, 15, 10)), slug: SLUGS.shift, max_registrations: 12)
+  seasons.each do |season|
+    season.events.create!(title: "Vert & Or", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 8, 29, 10)), slug: SLUGS.shift, max_registrations: 12).tap { seed_registrations(4, _1) }
+    season.events.create!(title: "Vert & Or", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 9, 10, 10)), slug: SLUGS.shift, max_registrations: 12).tap { seed_registrations(8, _1) }
+    season.events.create!(title: "Vert & Or", start_at: next_saturday_on_or_after(tz.local(season.name.to_i, 10, 1, 10)), slug: SLUGS.shift, max_registrations: 12)
+    season.events.create!(title: "Vert & Or", start_at: next_saturday_on_or_after(tz.local(season.name.to_i, 10, 15, 10)), slug: SLUGS.shift, max_registrations: 12)
 
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 9)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(6, _1) }
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 11)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 13)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 15)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 9)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(6, _1) }
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 11)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 13)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 4, 15)), slug: SLUGS.shift, max_registrations: 6)
 
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 9)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(5, _1) }
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 11)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(2, _1) }
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 13)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(5, _1) }
-  season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 15)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(1, _1) }
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 9)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(5, _1) }
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 11)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(2, _1) }
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 13)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(5, _1) }
+    season.events.create!(title: "Cannet-🌎-thon des fêtes", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 1, 5, 15)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(1, _1) }
 
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 9)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 11)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 13)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 15)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(3, _1, Faker::Internet.email) }
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 9)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 11)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 13)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 1, 15)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(3, _1, Faker::Internet.email) }
 
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 9)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 11)), slug: SLUGS.shift, max_registrations: 6)
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 13)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(2, _1, Faker::Internet.email) }
-  season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 15)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 9)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 11)), slug: SLUGS.shift, max_registrations: 6)
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 13)), slug: SLUGS.shift, max_registrations: 6).tap { seed_registrations(2, _1, Faker::Internet.email) }
+    season.events.create!(title: "Cannet-🌎-thon de Pâques", start_at: next_saturday_on_or_after(tz.local(season.name.to_i.succ, 5, 2, 15)), slug: SLUGS.shift, max_registrations: 6)
 
-  season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 11, 17, 18)), slug: SLUGS.shift, max_registrations: 8)
-  season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 12, 21, 20)), slug: SLUGS.shift, max_registrations: 8).tap { seed_registrations(3, _1) }
-  season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i.succ, 2, 24, 16)), slug: SLUGS.shift, max_registrations: 8)
-  season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i.succ, 3, 8, 17)), slug: SLUGS.shift, max_registrations: 8)
+    season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 11, 17, 18)), slug: SLUGS.shift, max_registrations: 8)
+    season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i, 12, 21, 20)), slug: SLUGS.shift, max_registrations: 8).tap { seed_registrations(3, _1) }
+    season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i.succ, 2, 24, 16)), slug: SLUGS.shift, max_registrations: 8)
+    season.events.create!(title: "Phoenix", start_at: next_sunday_on_or_after(tz.local(season.name.to_i.succ, 3, 8, 17)), slug: SLUGS.shift, max_registrations: 8)
 
-  season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 27, 22), slug: SLUGS.shift, max_registrations: 10)
-  season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 28, 22), slug: SLUGS.shift, max_registrations: 10).tap { seed_registrations(9, _1) }
-  season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 29, 22), slug: SLUGS.shift, max_registrations: 10).tap { seed_registrations(10, _1) }
-  season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 30, 22), slug: SLUGS.shift, max_registrations: 10)
-  season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 7, 1, 22), slug: SLUGS.shift, max_registrations: 10)
+    season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 27, 22), slug: SLUGS.shift, max_registrations: 10)
+    season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 28, 22), slug: SLUGS.shift, max_registrations: 10).tap { seed_registrations(9, _1) }
+    season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 29, 22), slug: SLUGS.shift, max_registrations: 10).tap { seed_registrations(10, _1) }
+    season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 6, 30, 22), slug: SLUGS.shift, max_registrations: 10)
+    season.events.create!(title: "Pif", start_at: tz.local(season.name.to_i.succ, 7, 1, 22), slug: SLUGS.shift, max_registrations: 10)
+  end
 end
