f55f1b476613b2d15502aaf9fb1b8614df65a65e
François Beausoleil <francois@teksol.info>
2025-03-29 09:42:53 -0400

    Attendance relationship back to registration isn't all that useful
    And it caused issues during import due to people attending that hadn't
    registered beforehand.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,6 @@ class Attendance < ApplicationRecord
   include Slugify
 
   belongs_to :event
-  belongs_to :registration, primary_key: %i[event_id person_name], foreign_key: %i[event_id person_name]
   validates :person_name, presence: true, length: {in: 1..200}
 
   def self.ransackable_attributes(auth_object = nil)
